d93e8cc1de3803836c999de8e881f81b393f3550
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-01 16:32:04 -0600

    emailValidator

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -12,10 +12,24 @@ export class EmailValidatorService implements AsyncValidator{
   validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
     const email = control.value;
 
+    const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber) => {
+      if(email === 'rod@mail.com'){
+        subscriber.next({emailTaken: true});
+        subscriber.complete();
+      }
+
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe( delay(3000) );
+    return httpCallObservable;
+  }
+  /*validate(control: AbstractControl<any, any>): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> {
+    const email = control.value;
+
     return of({
       emailTaken: true
     }).pipe( delay(2000) );
-  }
+  }*/
   /*registerOnValidatorChange?(fn: () => void): void {
     throw new Error('Method not implemented.');
   }*/
