b4a73686f71a703d07ac1f4f3712dee7edc2faf9
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-07-01 09:52:53 -0600

    Register form n validator functions

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    matchpass: ['', [Validators.required]],
+  });
 
+  constructor( private fb : FormBuilder ) { }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
+
+  isValidField(field:string){
+    //TODO: obtener validacion de un servicio
+  }
 }
